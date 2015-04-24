625506338c2a436fe00e38e9d0c40d643090758a
Limon Lin <limon7@gmail.com>
2015-04-24 14:24:03 +0800

    CH04 nth-root

--- 04/powers_tail.ex
+++ 04/powers_tail.ex
@@ -6,6 +6,15 @@ defmodule Powers do
   def do_raise(x, n, acc) do
     do_raise(x, n - 1, acc * x)
   end
+
+  def nth_root(x, n), do: do_nth_root(x, n, x / 2)
+  def do_nth_root(x, n, a) do
+    next = a - (raise(a, n) - x) / (n * raise(a, (n - 1)))
+    case (next - a) do
+      delta when abs(delta) < 1.0e-8 -> next
+      _ -> do_nth_root(x, n, next)
+    end
+  end
 end
 
 ExUnit.start
@@ -19,5 +28,10 @@ defmodule PowersTest do
     assert Powers.raise(1.2, 3) == 1.728
     assert Powers.raise(2, 0) == 1
     assert Powers.raise(2, -3) == 0.125
+  end
+
+  test "compute nth_root" do
+    assert Powers.nth_root(27, 3) == 3.0
+    assert Powers.nth_root(2, 2) == 1.4142135623730951
   end
 end
